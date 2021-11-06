a368d8cf79b6e66b41da0cb82d1ed7e084885b55
Sven <svenwu996@gmail.com>
2021-11-05 17:55:05 -0700

    updated bidoof

--- bidoof_script.js
+++ bidoof_script.js
@@ -1,6 +1,33 @@
 console.log("BIDOOF WAS HERE");
 
+let count = 0
+let countEl = document.getElementById("count-el")
+
+let saveEL = document.getElementById("save-el")
+
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
+
+function touch(){
+    // alert('You touched the Bidoof!');
+    console.log("Button was clicked")
+    count = count+1
+    countEl.textContent = count
+    console.log(count)
+}
+
+function save() {
+    let saveElText = count + " - "
+    saveEL.textContent += saveElText
+
+    console.log(count)
+
+    //Reset count to 0
+    countEl.textContent = 0
+    count = 0
+}
+
 // Creating an Event Listener when you touch Bidoof
-document.getElementById('bidoof_box').addEventListener('click', function(){
-    alert('You touched the Bidoof!');
-})
+document.getElementById('bidoof_box').addEventListener('click', touch, save, play)
